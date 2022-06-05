eeb6948ccadcdd5ad691c6f017ed03885be5820e
EduardoRDS-dev <Eduardo_Rodrigues_Santos@protonmail.ch>
2022-06-05 19:41:54 -0300

    add basic static componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import NavBar from 'componentes/NavBar';
+import DataTable from 'componentes/DataTable';
+import Footer from 'componentes/Footer'
+
 function App() {
   return (
-    <div>
-        
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
