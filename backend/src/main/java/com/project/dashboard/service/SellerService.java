1e05b9146f45d6b9fcb025dc67180e4387ef4f64
EduardoRDS-dev <Eduardo_Rodrigues_Santos@protonmail.ch>
2022-06-08 03:34:57 -0300

    layers

---
+++ backend/src/main/java/com/project/dashboard/service/SellerService.java
@@ -0,0 +1,22 @@
+package com.project.dashboard.service;
+
+import com.project.dashboard.dto.SellerDTO;
+import com.project.dashboard.entities.Seller;
+import com.project.dashboard.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SellerService {
+
+    @Autowired
+    private SellerRepository repository;
+
+    public List<SellerDTO> findAll(){
+        List<Seller> result = repository.findAll();
+        return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+    }
+}
